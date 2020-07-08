5c9c01191bcb69c93e546fd8e8d11607331b0bd1
Wink Saville <wink@saville.com>
2020-07-08 14:53:29 -0700

    Changed to collar with lip bracing.

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Experiment with internal bracing for wings
 This is an ellipse with a brace at about 45 degs and with Jeremy's
 help from advice in this [thread](https://groups.google.com/g/cadquery/c/dzsvo9R3XdY/m/gUctOXWABAAJ) it's now working.
 
-Need to "tune" the slicer settings and probably the bracing to print
-better, but still a success.
+I experimented with three types of bracing. The first was a plate with lightening holes.
+The second was just a collar around the inside of the tube. The third, the present
+version, is a collar with a lip. This last version is much stronger than the other two
+and I tuned the dimensions so it printed fairly well with a 0.4mm nozzle and PLA.
 
 Wirtten in python using the [CadQuery](https://github.com/CadQuery/cadquery) framework.
--- bracedTube.py
+++ bracedTube.py
@@ -1,10 +1,22 @@
 import cadquery as cq # type: ignore
 
 scale = 20
-wt = 0.4
 height = 2*scale
 xAxis = 1.0*scale
 yAxis = 0.5*scale
+wt = 0.4 # "Single Layer"
+
+collarThickness = wt + 0.1 # At wt of 0.4 this one layer
+                           # and prusa-slicer shows a gap
+                           # between collar and lip.
+                           # Adding 0.1 makes it two layers
+                           # and there is no gap
+
+braceAngle = 41
+braceZoffset = height/2
+lenBraceCollar = wt * 6
+lenBraceLip = wt * 6
+widthBraceLip = 2 * wt
 
 # Create a rod
 ellipticalRod = (
@@ -17,7 +29,7 @@ log(f'ellipticalRod.isValid()={ellipticalRod.val().isValid()}')
 
 # Cutter to hollow out the rod
 cutter = (
-    cq.Workplane("XY", origin=(0, 0))#(height+wt)/2))
+    cq.Workplane("XY", origin=(0, 0))
     .ellipse(xAxis-wt, yAxis-wt)
     .extrude(height+wt)
 )
@@ -29,18 +41,14 @@ ellipticalTube = ellipticalRod.cut(cutter)
 log(f'ellipticalTube.isValid()={ellipticalTube.val().isValid()}')
 #show_object(ellipticalTube)
 
-# Create a plate with some holes in it to lighten it.
+# Create a plate for the collar
 plate = (
-    cq.Workplane("YZ", origin=(0, 0))
-    .transformed(rotate=(-47, 0, ), offset=(0, height/2))
-    .center(0, 0)
-    .rect(25, 60)
-    .circle(8)
-    .center(0, -16)
-    .circle(6)
-    .center(0, 32)
-    .circle(6)
-    .extrude(wt)
+    cq.Workplane("XY", origin=(0, 0, braceZoffset))
+    .transformed(
+        rotate=(0, braceAngle, 0),
+    )
+    .rect(60, 25)
+    .extrude(collarThickness) # At 0.5 this is two layers, one layer at 0.4 but gaps
 )
 log(f'plate.val().isValid={plate.val().isValid()}')
 #show_object(plate)
@@ -50,6 +58,71 @@ brace = plate.cut(ellipticalTube).solids().last()
 log(f'brace.isValid()={brace.val().isValid()}')
 #show_object(brace)
 
-bracedTube = brace.union(ellipticalTube)
+# brace collar
+braceCollarCutter = (
+    cq.Workplane("XY", origin=(0, 0))
+    .ellipse(xAxis-lenBraceCollar, yAxis-lenBraceCollar)
+    .extrude(height)
+)
+log(f'braceCollarCutter.isValid()={braceCollarCutter.val().isValid()}')
+#show_object(braceCollarCutter)
+#)
+braceCollar = brace.cut(braceCollarCutter)
+show_object(braceCollar)
+
+# Brace lip Rod. Initially "braceLipRod=braceCollarCutter" but
+# prusa-slicer ended up with a gap between the collar and lip
+# So I make the braceLipRod slightly bigger
+#braceLipRod = braceCollarCutter
+braceLipRod = (
+    cq.Workplane("XY", origin=(0, 0))
+    #.ellipse(xAxis-lenBraceCollar+(1*wt), yAxis-lenBraceCollar+(1*wt))
+    .ellipse(xAxis-lenBraceCollar+0.5, yAxis-lenBraceCollar+0.5)
+    .extrude(height)
+)
+log(f'braceLipRod.isValid()={braceLipRod.val().isValid()}')
+#show_object(braceLipRod)
+
+# Brace lip cutter
+braceLipCutter = (
+    cq.Workplane("XY", origin=(0, 0))
+    .ellipse(xAxis-lenBraceCollar-widthBraceLip, yAxis-lenBraceCollar-widthBraceLip)
+    .extrude(height)
+)
+log(f'braceLipCutter.isValid()={braceLipCutter.val().isValid()}')
+#show_object(braceLipCutter)
+
+# Brace lib tube
+braceLipTube = braceLipRod.cut(braceLipCutter)
+log(f'braceLipTube.isValid()={braceLipTube.val().isValid()}')
+#show_object(braceLipTube)
+#show_object(braceCollar)
+
+## Brace lip Splitters
+braceLipTop = (
+    braceLipTube
+    .faces(">Z")
+    .workplane(-height/2)
+    .transformed(
+        rotate=(0, braceAngle),
+    )
+    .split(keepTop=True, keepBottom=False)
+)
+log(f'braceLipTop.isValid()={braceLipTop.val().isValid()}')
+#show_object(braceLipTop)
+
+braceLip = (
+    braceLipTop
+    .faces(">Z")
+    .workplane(-height/2 + lenBraceLip)
+    .transformed(
+        rotate=(0, braceAngle),
+    )
+    .split(keepTop=False, keepBottom=True)
+)
+log(f'braceLip.isValid()={braceLip.val().isValid()}')
+#show_object(braceLip)
+
+bracedTube = ellipticalTube.union(braceCollar).union(braceLip)
 log(f'bracedTube.isValid()={bracedTube.val().isValid()}')
 show_object(bracedTube)
